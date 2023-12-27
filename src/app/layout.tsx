55c9354b37519436cc341a7599821084b56d270c
Salman Farshee <salman@aveniripltd.com>
2023-12-27 15:51:43 +0600

    added footer section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
-        {children}
+        <>
+          {children}
+          <Footer />
+        </>
       </body>
     </html>
   );
